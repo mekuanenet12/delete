1bb2afa3433b7512b55cd50e258c96004ca9e8cc
mekuanenet12 <mekuanenet.anteneh1@t-mobile.com>
2021-12-24 19:18:26 -0800

    Update DemoController.java

--- src/main/java/com/example/demoGit/DemoController.java
+++ src/main/java/com/example/demoGit/DemoController.java
@@ -1,5 +1,8 @@
 package com.example.demoGit;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class DemoController {
 
